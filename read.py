1fa2066d874c6fc63358094dba3b084d2d8e0c78
Yuying Lee <yyleee@gmail.com>
2023-11-20 21:34:41 +0800

    add basic read.py

---
+++ read.py
@@ -0,0 +1,17 @@
+data = []
+count = 0
+with open('reviews.txt.crdownload', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+
+print(len(data))
+
+print(data[0])
+print('-----------')
+print(data[1])
+
+
+
